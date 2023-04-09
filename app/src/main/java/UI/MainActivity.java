34112f7476c9b86ad9fdbcf3730f4b24d4b20838
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-09 16:03:03 -0400

    Added 'View Schedule' button functionality, comments and Javadocs

---
+++ app/src/main/java/UI/MainActivity.java
@@ -0,0 +1,40 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.student_scheduler.R;
+import java.util.Objects;
+
+/**
+ * This is the main activity in the student scheduler.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // View Schedule button
+        Button button = findViewById(R.id.view_schedule);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Method called when view schedule button is clicked.
+             *
+             * @param view The view that was clicked.
+             */
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TermList.class);
+                startActivity(intent);
+            }
+        });
+
+        // Hide action bar on splash screen
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
