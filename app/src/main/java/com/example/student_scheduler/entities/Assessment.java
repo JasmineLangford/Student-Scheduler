2cff03ece22077c6d6943769aa348e3667326b60
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-10 16:15:49 -0400

    Added variables with constructor and getter/setter

---
+++ app/src/main/java/com/example/student_scheduler/entities/Assessment.java
@@ -0,0 +1,52 @@
+package com.example.student_scheduler.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName="assessment")
+public class Assessment {
+    @PrimaryKey(autoGenerate = true)
+    private int assessmentID;
+    private String assessmentType;
+    private String assessmentTitle;
+    private String assessmentEndDate;
+
+    public Assessment(int assessmentID, String assessmentType, String assessmentTitle, String assessmentEndDate) {
+        this.assessmentID = assessmentID;
+        this.assessmentType = assessmentType;
+        this.assessmentTitle = assessmentTitle;
+        this.assessmentEndDate = assessmentEndDate;
+    }
+
+    public int getAssessmentID() {
+        return assessmentID;
+    }
+
+    public void setAssessmentID(int assessmentID) {
+        this.assessmentID = assessmentID;
+    }
+
+    public String getAssessmentType() {
+        return assessmentType;
+    }
+
+    public void setAssessmentType(String assessmentType) {
+        this.assessmentType = assessmentType;
+    }
+
+    public String getAssessmentTitle() {
+        return assessmentTitle;
+    }
+
+    public void setAssessmentTitle(String assessmentTitle) {
+        this.assessmentTitle = assessmentTitle;
+    }
+
+    public String getAssessmentEndDate() {
+        return assessmentEndDate;
+    }
+
+    public void setAssessmentEndDate(String assessmentEndDate) {
+        this.assessmentEndDate = assessmentEndDate;
+    }
+}
---
+++ app/src/main/java/com/example/student_scheduler/entities/Course.java
@@ -0,0 +1,101 @@
+package com.example.student_scheduler.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName="course")
+public class Course {
+    @PrimaryKey(autoGenerate = true)
+    private int courseID;
+    private String courseTitle;
+    private String courseStartDate;
+    private String courseEndDate;
+    private String courseStatus;
+    private String instructorName;
+    private String instructorPhone;
+    private String instructorEmail;
+    private String courseNote;
+
+    public Course(int courseID, String courseTitle, String courseStartDate, String courseEndDate, String courseStatus, String instructorName, String instructorPhone, String instructorEmail, String courseNote) {
+        this.courseID = courseID;
+        this.courseTitle = courseTitle;
+        this.courseStartDate = courseStartDate;
+        this.courseEndDate = courseEndDate;
+        this.courseStatus = courseStatus;
+        this.instructorName = instructorName;
+        this.instructorPhone = instructorPhone;
+        this.instructorEmail = instructorEmail;
+        this.courseNote = courseNote;
+    }
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getCourseStartDate() {
+        return courseStartDate;
+    }
+
+    public void setCourseStartDate(String courseStartDate) {
+        this.courseStartDate = courseStartDate;
+    }
+
+    public String getCourseEndDate() {
+        return courseEndDate;
+    }
+
+    public void setCourseEndDate(String courseEndDate) {
+        this.courseEndDate = courseEndDate;
+    }
+
+    public String getCourseStatus() {
+        return courseStatus;
+    }
+
+    public void setCourseStatus(String courseStatus) {
+        this.courseStatus = courseStatus;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorPhone() {
+        return instructorPhone;
+    }
+
+    public void setInstructorPhone(String instructorPhone) {
+        this.instructorPhone = instructorPhone;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    public String getCourseNote() {
+        return courseNote;
+    }
+
+    public void setCourseNote(String courseNote) {
+        this.courseNote = courseNote;
+    }
+}
