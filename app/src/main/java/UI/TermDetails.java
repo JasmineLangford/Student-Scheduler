b044c4de91f9b27fbee870718269c85b29e71fee
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-10 01:09:53 -0400

    Added Javadocs

--- app/src/main/java/UI/TermDetails.java
+++ app/src/main/java/UI/TermDetails.java
@@ -9,8 +9,10 @@ import android.os.Bundle;
 import com.example.student_scheduler.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+/**
+ * This activity allows the user to view details for a selected term.
+ */
 public class TermDetails extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +22,15 @@ public class TermDetails extends AppCompatActivity {
         fabTermDetails.setOnClickListener(view -> showTermSelections());
     }
 
+    /**
+     * This method is a dialog box that shows the selections available to the user once the floating
+     * action button is clicked. The user has the option to modify, add or delete a term.
+     */
     public void showTermSelections() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("")
+
+                // User selections for floating action button
                 .setItems(new String[]{"Modify Term", "Add New Term", "Delete Term"},(dialog, which) -> {
                     Intent intent = new Intent(TermDetails.this, TermModify.class);
                     startActivity(intent);
