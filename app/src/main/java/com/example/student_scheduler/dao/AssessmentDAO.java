d64133b61201bd3fc728becc205bc7ce356ad25f
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-10 16:57:30 -0400

    Created DAO interfaces for database access

---
+++ app/src/main/java/com/example/student_scheduler/dao/AssessmentDAO.java
@@ -0,0 +1,30 @@
+package com.example.student_scheduler.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.student_scheduler.entities.Assessment;
+
+import java.util.List;
+
+@Dao
+public interface AssessmentDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Assessment assessment);
+
+    @Update
+    void update(Assessment assessment);
+
+    @Delete
+    void delete(Assessment assessment);
+
+    @Query("SELECT * FROM ASSESSMENT ORDER BY assessmentID ASC")
+    List<Assessment> getAllAssessments();
+
+    @Query("SELECT * FROM ASSESSMENT WHERE assessmentID= :assessmentID ORDER BY assessmentID ASC")
+    List<Assessment> getAssociatedAssessments(int assessmentID);
+}
---
+++ app/src/main/java/com/example/student_scheduler/dao/CourseDAO.java
@@ -0,0 +1,28 @@
+package com.example.student_scheduler.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.example.student_scheduler.entities.Course;
+import java.util.List;
+
+@Dao
+public interface CourseDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Course course);
+
+    @Update
+    void update(Course course);
+
+    @Delete
+    void delete(Course course);
+
+    @Query("SELECT * FROM COURSE ORDER BY courseID ASC")
+    List<Course> getAllCourses();
+
+    @Query("SELECT * FROM COURSE WHERE courseID= :courseID ORDER BY courseID ASC")
+    List<Course> getAssociatedCourses(int courseID);
+}
