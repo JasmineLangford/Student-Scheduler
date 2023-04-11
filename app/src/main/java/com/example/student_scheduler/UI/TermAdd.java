60ad6d9d70d8e25e493fdd55106b2cb3a44afea5
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-11 11:45:50 -0400

    Created new activity

---
+++ app/src/main/java/com/example/student_scheduler/UI/TermAdd.java
@@ -0,0 +1,31 @@
+package com.example.student_scheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.student_scheduler.R;
+
+public class TermAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_add);
+    }
+
+        /**
+         * This method handles the click event for the back button in the action bar. When the back
+         * button is clicked, `onBackPressed()` is called to go back to the previous activity.
+         */
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    onBackPressed();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+}
