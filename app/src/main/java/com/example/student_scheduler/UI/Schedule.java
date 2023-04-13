e9a50bdadf86d022d6cd63e072d6cd8a4bc1767e
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-13 15:31:51 -0400

    Added new activity for full schedule UI

---
+++ app/src/main/java/com/example/student_scheduler/UI/Schedule.java
@@ -0,0 +1,43 @@
+package com.example.student_scheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.student_scheduler.R;
+
+import java.util.Objects;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * This method handles the click event for the back button in the action bar. When the back
+     * button is clicked, `onBackPressed()` is called to go back to the previous activity.
+     */
+    @SuppressLint("NonConstantResourceId")
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
