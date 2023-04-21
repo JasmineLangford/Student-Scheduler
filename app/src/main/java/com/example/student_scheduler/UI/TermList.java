8a2195592954f48eee4c41eec639377185bb4974
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-20 22:08:38 -0400

    Added new activities

--- app/src/main/java/com/example/student_scheduler/UI/TermList.java
+++ app/src/main/java/com/example/student_scheduler/UI/TermList.java
@@ -2,12 +2,13 @@ package com.example.student_scheduler.UI;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
-
 import com.example.student_scheduler.R;
+import com.example.student_scheduler.database.Repository;
+import com.example.student_scheduler.entities.Term;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
@@ -17,17 +18,29 @@ import java.util.Objects;
  * the expand arrow is clicked.
  */
 public class TermList extends AppCompatActivity {
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
 
-        // ToDo: Recycler view for list of terms
+        // SAMPLE DATA
+        Term term = new Term(0, "Term 1", "01/01/2023","06/20/2023");
+        Repository repository = new Repository(getApplication());
+
+        // Display current terms from repository
+//        RecyclerView termListRecycler = findViewById(R.id.term_list_recycler);
+//        final TermAdapter termAdapter = new TermAdapter(this);
+//        termListRecycler.setAdapter(termAdapter);
+//        termListRecycler.setLayoutManager(new LinearLayoutManager(this));
+//        repository=new Repository(getApplication());
+//        List<Term> allTerms = repository.getAllTerms();
+//        termAdapter.setTerms(allTerms);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fabTermDetails = findViewById(R.id.FAB_add_term);
+        FloatingActionButton fabTermDetails = findViewById(R.id.FAB_add_course);
         fabTermDetails.setOnClickListener(view -> showAddTermDialog());
     }
 
@@ -45,11 +58,15 @@ public class TermList extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method contains the option to add a new term when the user clicks the floating action
+     * button.
+     */
     public void showAddTermDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("")
                 .setItems(new String[]{"Add New Term"}, (dialog, which) -> {
-                    Intent intent = new Intent(TermList.this, TermAdd.class);
+                    Intent intent = new Intent(TermList.this, AddTerm.class);
                     startActivity(intent);
                 });
         builder.create().show();
