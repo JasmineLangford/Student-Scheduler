eaf828234b96c7e444c8e055827d01a22f1cef3c
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-20 22:54:38 -0400

    Added navigation

--- app/src/main/java/com/example/student_scheduler/UI/AssessmentList.java
+++ app/src/main/java/com/example/student_scheduler/UI/AssessmentList.java
@@ -2,9 +2,10 @@ package com.example.student_scheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.student_scheduler.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AssessmentList extends AppCompatActivity {
 
@@ -12,5 +13,12 @@ public class AssessmentList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_list);
+
+        FloatingActionButton assessmentFAB = findViewById(R.id.FAB_add_assessment);
+        assessmentFAB.setOnClickListener(view -> {
+            Intent intent = new Intent(AssessmentList.this,AssessmentDetails.class);
+            startActivity(intent);
+        });
+
     }
 }
