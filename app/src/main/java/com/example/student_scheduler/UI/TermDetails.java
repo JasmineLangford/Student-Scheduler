dd6928472d388f8137c28b8cbf772abab268f8b5
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-23 14:57:35 -0400

    Cleaned up code and added Javadocs

--- app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
+++ app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
@@ -2,17 +2,23 @@ package com.example.student_scheduler.UI;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.student_scheduler.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Objects;
 
 /**
- * This activity allows the user to view details for a selected term.
+ * This activity allows the user to view details for a selected term. The data is populated into
+ * EditText fields for the user to modify or delete. If there are associated courses with this term,
+ * the user will not be allowed to delete the term until all courses have been deleted.
+ * <p>
+ * The user can also use the floating action button in the bottom right-hand corner to view
+ * additional options related to the term.
  */
 public class TermDetails extends AppCompatActivity {
 
@@ -24,16 +30,9 @@ public class TermDetails extends AppCompatActivity {
         // Display toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        //
+        // FAB options for selection term
         FloatingActionButton term_options = findViewById(R.id.FAB_term_options);
-        term_options.setOnClickListener(view -> {
-            showTermOptionDialog();
-        });
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
+        term_options.setOnClickListener(view -> showTermOptionDialog());
     }
 
     /**
@@ -51,16 +50,16 @@ public class TermDetails extends AppCompatActivity {
     }
 
     /**
-     * This method contains the option to add a new term when the user clicks the floating action
-     * button.
+     * This method is called when the floating action button is clicked. This alert dialog provides
+     * the user two options: 1) adding a new term or 2) viewing courses related to the selected term.
      */
     public void showTermOptionDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("")
                 .setItems(new String[]{"Add New Term", "View Courses"}, (dialog, which) -> {
-                    if(which == 0){
-                    Intent intent = new Intent(TermDetails.this, AddTerm.class);
-                    startActivity(intent);
+                    if (which == 0) {
+                        Intent intent = new Intent(TermDetails.this, AddTerm.class);
+                        startActivity(intent);
                     } else if (which == 1) {
                         Intent intent = new Intent(TermDetails.this, CourseList.class);
                         startActivity(intent);
