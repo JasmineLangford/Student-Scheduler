ea7408088a0b3d3c02e67a8cee8adb955c30aa84
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-23 03:44:21 -0400

    Working on modifying existing data from database

--- app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
+++ app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import com.example.student_scheduler.R;
+import com.example.student_scheduler.database.Repository;
+import com.example.student_scheduler.entities.Term;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.Objects;
 
@@ -13,19 +17,58 @@ import java.util.Objects;
  * This activity allows the user to view details for a selected term.
  */
 public class TermDetails extends AppCompatActivity {
+    EditText editTermTitle;
+    EditText editTermStart;
+    EditText editTermEnd;
+    String termTitleEdit;
+    String termStartEdit;
+    String termEndEdit;
+    int termID;
+    Term term;
+    Repository repository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
 
+        // Editable text fields
+        editTermTitle = findViewById(R.id.term_title);
+        editTermStart = findViewById(R.id.start_date);
+        editTermEnd = findViewById(R.id.end_date);
+        termTitleEdit = getIntent().getStringExtra("term_title");
+        termStartEdit = getIntent().getStringExtra("term_start");
+        termEndEdit = getIntent().getStringExtra("term_end");
+        editTermTitle.setText(termTitleEdit);
+        editTermStart.setText(termStartEdit);
+        editTermEnd.setText(termEndEdit);
+
+        // Save fields
+        repository = new Repository(getApplication());
+        termID = getIntent().getIntExtra("term_id",-1);
+        Button button = findViewById(R.id.save_new_term);
+        button.setOnClickListener(view -> {
+            if (termID == -1) {
+                term = new Term(0,editTermTitle.getText().toString(),
+                        editTermStart.getText().toString(),editTermEnd.getText().toString());
+                repository.insert(term);
+            }
+            else {
+                term = new Term(0,editTermTitle.getText().toString(),
+                        editTermStart.getText().toString(),editTermEnd.getText().toString());
+                repository.update(term);
+            }
+        });
+
+        // Display toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        //
         FloatingActionButton coursesFAB = findViewById(R.id.to_course_details);
         coursesFAB.setOnClickListener(view -> {
             Intent intent = new Intent(TermDetails.this,CourseDetails.class);
             startActivity(intent);
         });
-//        FloatingActionButton fabCourseDetails = findViewById(R.id.to_course_details);
-//        fabCourseDetails.setOnClickListener(view -> addNewCourse());
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,19 +90,4 @@ public class TermDetails extends AppCompatActivity {
         }
     }
 
-//    /**
-//     * This method is a dialog box that shows the selections available to the user once the floating
-//     * action button is clicked. The user has the option to modify, add or delete a term.
-//     */
-//    public void addNewCourse() {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle("")
-//
-//                // User selections for floating action button
-//                .setItems(new String[]{"Add New Course"}, (dialog, which) -> {
-//                    Intent intent = new Intent(TermDetails.this, CourseDetails.class);
-//                    startActivity(intent);
-//                });
-//        builder.create().show();
-//    }
 }
