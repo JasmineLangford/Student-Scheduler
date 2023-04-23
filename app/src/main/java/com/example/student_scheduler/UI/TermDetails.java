15b8baa3ad4d58b78da74c8cd1922d1dd87639e8
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-23 12:13:49 -0400

    Updated UI and navigation; working on modify and delete functionality

--- app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
+++ app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
@@ -1,15 +1,13 @@
 package com.example.student_scheduler.UI;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
+
 import com.example.student_scheduler.R;
-import com.example.student_scheduler.database.Repository;
-import com.example.student_scheduler.entities.Term;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.Objects;
 
@@ -17,57 +15,19 @@ import java.util.Objects;
  * This activity allows the user to view details for a selected term.
  */
 public class TermDetails extends AppCompatActivity {
-    EditText editTermTitle;
-    EditText editTermStart;
-    EditText editTermEnd;
-    String termTitleEdit;
-    String termStartEdit;
-    String termEndEdit;
-    int termID;
-    Term term;
-    Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
 
-        // Editable text fields
-        editTermTitle = findViewById(R.id.term_title);
-        editTermStart = findViewById(R.id.start_date);
-        editTermEnd = findViewById(R.id.end_date);
-        termTitleEdit = getIntent().getStringExtra("term_title");
-        termStartEdit = getIntent().getStringExtra("term_start");
-        termEndEdit = getIntent().getStringExtra("term_end");
-        editTermTitle.setText(termTitleEdit);
-        editTermStart.setText(termStartEdit);
-        editTermEnd.setText(termEndEdit);
-
-        // Save fields
-        repository = new Repository(getApplication());
-        termID = getIntent().getIntExtra("term_id",-1);
-        Button button = findViewById(R.id.save_new_term);
-        button.setOnClickListener(view -> {
-            if (termID == -1) {
-                term = new Term(0,editTermTitle.getText().toString(),
-                        editTermStart.getText().toString(),editTermEnd.getText().toString());
-                repository.insert(term);
-            }
-            else {
-                term = new Term(0,editTermTitle.getText().toString(),
-                        editTermStart.getText().toString(),editTermEnd.getText().toString());
-                repository.update(term);
-            }
-        });
-
         // Display toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         //
-        FloatingActionButton coursesFAB = findViewById(R.id.to_course_details);
-        coursesFAB.setOnClickListener(view -> {
-            Intent intent = new Intent(TermDetails.this,CourseDetails.class);
-            startActivity(intent);
+        FloatingActionButton term_options = findViewById(R.id.FAB_term_options);
+        term_options.setOnClickListener(view -> {
+            showTermOptionDialog();
         });
     }
 
@@ -90,4 +50,22 @@ public class TermDetails extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method contains the option to add a new term when the user clicks the floating action
+     * button.
+     */
+    public void showTermOptionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("")
+                .setItems(new String[]{"Add New Term", "View Courses"}, (dialog, which) -> {
+                    if(which == 0){
+                    Intent intent = new Intent(TermDetails.this, AddTerm.class);
+                    startActivity(intent);
+                    } else if (which == 1) {
+                        Intent intent = new Intent(TermDetails.this, CourseList.class);
+                        startActivity(intent);
+                    }
+                });
+        builder.create().show();
+    }
 }
