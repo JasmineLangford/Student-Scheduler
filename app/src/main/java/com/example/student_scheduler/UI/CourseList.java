0f85856a4446d06bcfa35c173b7c5435f7e7d9b3
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-24 23:51:55 -0400

    Cleaned up code

--- app/src/main/java/com/example/student_scheduler/UI/CourseList.java
+++ app/src/main/java/com/example/student_scheduler/UI/CourseList.java
@@ -47,18 +47,4 @@ public class CourseList extends AppCompatActivity {
             startActivity(intent);
         });
     }
-//
-//    /**
-//     * This method handles the click event for the back button in the action bar. When the back
-//     * button is clicked, `onBackPressed()` is called to go back to the previous activity.
-//     */
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                onBackPressed();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
 }
