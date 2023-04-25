8ca42a3b291c8cc0b0705ad4e607c1a680ec4b73
Jasmine Langford <jkhatt0788@gmail.com>
2023-04-24 23:49:15 -0400

    Changed floating action button to extended floating action button

--- app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
+++ app/src/main/java/com/example/student_scheduler/UI/TermDetails.java
@@ -1,14 +1,18 @@
 package com.example.student_scheduler.UI;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupMenu;
 
 import com.example.student_scheduler.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import java.util.Objects;
 
@@ -16,23 +20,40 @@ import java.util.Objects;
  * This activity allows the user to view details for a selected term. The data is populated into
  * EditText fields for the user to modify or delete. If there are associated courses with this term,
  * the user will not be allowed to delete the term until all courses have been deleted.
- * <p>
+ *
  * The user can also use the floating action button in the bottom right-hand corner to view
  * additional options related to the term.
  */
 public class TermDetails extends AppCompatActivity {
+
+    EditText term_title;
+    EditText term_start;
+    EditText term_end;
+    String title;
+    String startDate;
+    String endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
 
+        term_title = findViewById(R.id.term_title_edit);
+        term_start = findViewById(R.id.term_start_edit);
+        term_end = findViewById(R.id.term_end_edit);
+        title = getIntent().getStringExtra("term_title");
+        startDate = getIntent().getStringExtra("term_start");
+        endDate = getIntent().getStringExtra("term_end");
+        term_title.setText(title);
+        term_start.setText(startDate);
+        term_end.setText(endDate);
+
         // Display toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        // FAB options for selection term
-        FloatingActionButton term_options = findViewById(R.id.FAB_term_options);
-        term_options.setOnClickListener(view -> showTermOptionDialog());
+        // Extended FAB with sub menu
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ExtendedFloatingActionButton termFab = findViewById(R.id.terms_extended_fab);
+        termFab.setOnClickListener(this::showSubMenu);
     }
 
     /**
@@ -53,18 +74,25 @@ public class TermDetails extends AppCompatActivity {
      * This method is called when the floating action button is clicked. This alert dialog provides
      * the user two options: 1) adding a new term or 2) viewing courses related to the selected term.
      */
-    public void showTermOptionDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("")
-                .setItems(new String[]{"Add New Term", "View Courses"}, (dialog, which) -> {
-                    if (which == 0) {
-                        Intent intent = new Intent(TermDetails.this, AddTerm.class);
-                        startActivity(intent);
-                    } else if (which == 1) {
-                        Intent intent = new Intent(TermDetails.this, CourseList.class);
-                        startActivity(intent);
-                    }
-                });
-        builder.create().show();
+    public void showSubMenu(View view) {
+        PopupMenu termPopupMenu = new PopupMenu (this, view);
+        termPopupMenu.getMenuInflater().inflate(R.menu.term_menu, termPopupMenu.getMenu());
+        termPopupMenu.setOnMenuItemClickListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.add_term:
+                    Intent toAddTerm = new Intent(TermDetails.this, AddTerm.class);
+                    startActivity(toAddTerm);
+                    break;
+                case R.id.view_courses:
+                    Intent toViewCourses = new Intent(TermDetails.this, CourseList.class);
+                    startActivity(toViewCourses);
+                    break;
+                case R.id.delete_term:
+                    // TODO: add delete functionality
+                    return true;
+            }
+            return false;
+        });
+        termPopupMenu.show();
     }
 }
