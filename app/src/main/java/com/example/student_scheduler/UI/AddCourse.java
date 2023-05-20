c13260c7112cba71f1c9921e0dcadefc5192331a
Jasmine Langford <jkhatt0788@gmail.com>
2023-05-20 13:05:58 -0400

    Modified UI; Working on add course functionality

--- app/src/main/java/com/example/student_scheduler/UI/AddCourse.java
+++ app/src/main/java/com/example/student_scheduler/UI/AddCourse.java
@@ -2,11 +2,14 @@ package com.example.student_scheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.student_scheduler.R;
 import com.example.student_scheduler.database.Repository;
@@ -19,23 +22,25 @@ import java.util.Objects;
  * This activity allows the user to add a new course to the database and displays the new course on
  * the screen with the listed courses once the Save button is clicked.
  */
-public class AddCourse extends AppCompatActivity {
+public class AddCourse extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    EditText editCourseTitle;
-    EditText editCourseStart;
-    EditText editCourseEnd;
-    EditText editCourseStatus;
-    EditText editInstructorName;
-    EditText editPhone;
-    EditText editEmail;
+    EditText addCourseTitle;
+    EditText addCourseStart;
+    EditText addCourseEnd;
+    Spinner addCourseStatus;
+    EditText addInstructorName;
+    EditText addPhone;
+    EditText addEmail;
+    EditText addCourseNotes;
 
     String courseTitleEdit;
     String courseStartEdit;
     String courseEndEdit;
-    String courseStatusEdit;
+    String[] courseStatusEdit;
     String instructorNameEdit;
     String phoneEdit;
     String emailEdit;
+    String courseNoteEdit;
 
     int courseID;
     int termID;
@@ -52,33 +57,45 @@ public class AddCourse extends AppCompatActivity {
         setContentView(R.layout.activity_add_course);
 
         // Editable text fields
-        editCourseTitle = findViewById(R.id.course_title_edit);
-        editCourseStart = findViewById(R.id.course_start_edit);
-        editCourseEnd = findViewById(R.id.course_end_edit);
-        editCourseStatus = findViewById(R.id.course_status_edit);
-        editInstructorName = findViewById(R.id.instructor_name_edit);
-        editEmail = findViewById(R.id.email_edit);
-        editPhone = findViewById(R.id.phone_edit);
+        addCourseTitle = findViewById(R.id.course_title_edit);
+        addCourseStart = findViewById(R.id.course_start_edit);
+        addCourseEnd = findViewById(R.id.course_end_edit);
+        addInstructorName = findViewById(R.id.instructor_name_edit);
+        addEmail = findViewById(R.id.email_edit);
+        addPhone = findViewById(R.id.phone_edit);
+        addCourseNotes = findViewById(R.id.course_notes_edit);
 
         courseTitleEdit = getIntent().getStringExtra("course_title");
         courseStartEdit = getIntent().getStringExtra("course_start");
         courseEndEdit = getIntent().getStringExtra("course_end");
-        courseStatusEdit = getIntent().getStringExtra("course_status");
+        courseStatusEdit = getIntent().getStringArrayExtra("course_status");
         instructorNameEdit = getIntent().getStringExtra("instructor_name");
         phoneEdit = getIntent().getStringExtra("instructor_phone");
         emailEdit = getIntent().getStringExtra("instructor_email");
-
-        editCourseTitle.setText(courseTitleEdit);
-        editCourseStart.setText(courseStartEdit);
-        editCourseEnd.setText(courseEndEdit);
-        editCourseStatus.setText(courseStatusEdit);
-        editInstructorName.setText(instructorNameEdit);
-        editPhone.setText(phoneEdit);
-        editEmail.setText(emailEdit);
+        courseNoteEdit = getIntent().getStringExtra("course_notes");
+        addCourseTitle.setText(courseTitleEdit);
+        addCourseStart.setText(courseStartEdit);
+        addCourseEnd.setText(courseEndEdit);
+        addInstructorName.setText(instructorNameEdit);
+        addPhone.setText(phoneEdit);
+        addEmail.setText(emailEdit);
+        addCourseNotes.setText(courseNoteEdit);
 
         // Cancel and go back to list of terms
         Button cancel = findViewById(R.id.cancel_button);
         cancel.setOnClickListener(view -> finish());
+
+        // Dropdown selection for course status
+        Spinner courseStatusSpinner = findViewById(R.id.course_status_spinner2);
+        ArrayAdapter<CharSequence> statusAdapter = ArrayAdapter.createFromResource(this,
+                R.array.status, android.R.layout.simple_spinner_item);
+        statusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        courseStatusSpinner.setAdapter(statusAdapter);
+        courseStatusSpinner.setOnItemSelectedListener(this);
+
+        String courseStatus = getIntent().getStringExtra("course_status");
+        int position = statusAdapter.getPosition(courseStatus);
+        courseStatusSpinner.setSelection(0);
 
         // Save fields
 //        repository = new Repository(getApplication());
@@ -111,12 +128,23 @@ public class AddCourse extends AppCompatActivity {
      * button is clicked, `onBackPressed()` is called to go back to the previous activity.
      */
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        repository = new Repository(getApplication());
+        String selectedStatus = adapterView.getItemAtPosition(position).toString();
+        course = new Course();
+        repository.insert(course);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
