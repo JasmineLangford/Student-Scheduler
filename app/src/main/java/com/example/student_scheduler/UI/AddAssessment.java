7c8b2b4e1a4bb75295ade3d47a906c69743b1c8d
Jasmine Langford <jkhatt0788@gmail.com>
2023-05-25 12:35:48 -0400

    DatePicker was added

--- app/src/main/java/com/example/student_scheduler/UI/AddAssessment.java
+++ app/src/main/java/com/example/student_scheduler/UI/AddAssessment.java
@@ -2,6 +2,7 @@ package com.example.student_scheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -14,6 +15,10 @@ import com.example.student_scheduler.R;
 import com.example.student_scheduler.database.Repository;
 import com.example.student_scheduler.entities.Assessment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -23,9 +28,11 @@ import java.util.Objects;
 
 public class AddAssessment extends AppCompatActivity {
 
-    EditText addAssessmentTitle;
-    EditText addAssessmentEnd;
+    Button endDatePicker;
+    DatePickerDialog.OnDateSetListener myDate;
+    final Calendar myCalendar = Calendar.getInstance();
 
+    EditText addAssessmentTitle;
     String assessmentTitleEdit;
     String assessmentEndEdit;
 
@@ -39,18 +46,41 @@ public class AddAssessment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assessment);
 
+        // DatePicker
+        endDatePicker = findViewById(R.id.end_date_picker);
+        String formattedDate = "MM/dd/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(formattedDate, Locale.US);
+        endDatePicker.setText("Select Date");
+        endDatePicker.setOnClickListener(view -> {
+            String info = endDatePicker.getText().toString();
+            try{
+                myCalendar.setTime(Objects.requireNonNull(simpleDateFormat.parse(info)));
+            }catch (ParseException e) {
+                e.printStackTrace();
+            }
+            new DatePickerDialog(AddAssessment.this,myDate,
+                    myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        });
+
+        myDate = (datePicker, year, monthOfYear, dayOfMonth) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDate();
+
+            String selectedDate = simpleDateFormat.format(myCalendar.getTime());
+            endDatePicker.setText(selectedDate);
+        };
+
         courseID = getIntent().getIntExtra("course_id", courseID);
         assessmentID = getIntent().getIntExtra("assessment_id",-1);
 
         // Editable fields
         addAssessmentTitle = findViewById(R.id.assessment_title_edit);
-        addAssessmentEnd = findViewById(R.id.assessment_end_edit);
-
         assessmentTitleEdit = getIntent().getStringExtra("assessment_title");
         assessmentEndEdit = getIntent().getStringExtra("assessment_end");
-
         addAssessmentTitle.setText(assessmentTitleEdit);
-        addAssessmentEnd.setText(assessmentEndEdit);
 
         // Cancel and go back to course details
         Button cancel = findViewById(R.id.cancel_button);
@@ -69,8 +99,8 @@ public class AddAssessment extends AppCompatActivity {
         saveAssessment.setOnClickListener(view -> {
             assessment = new Assessment(0,courseID,
                     addAssessmentTitle.getText().toString(),
-                    assessmentTypeSpinner.getSelectedItem().toString(),
-                    addAssessmentEnd.getText().toString());
+                    assessmentTypeSpinner.getSelectedItem().toString(),endDatePicker.getText().toString());
+                   //addAssessmentEnd.getText().toString());
             repository.insert(assessment);
 
             // Message to confirm add and back to term details
@@ -91,5 +121,13 @@ public class AddAssessment extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Update date for date picker
+    private void updateDate(){
+        String formattedDate = "MM/dd/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(formattedDate, Locale.US);
+
+        endDatePicker.setText(simpleDateFormat.format(myCalendar.getTime()));
     }
 }
