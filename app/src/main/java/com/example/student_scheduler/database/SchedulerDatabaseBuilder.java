54d585787c1924495fad81d38b79133fbdfe080f
Jasmine Langford <jkhatt0788@gmail.com>
2023-05-27 15:02:40 -0400

    Cleaned up code

--- app/src/main/java/com/example/student_scheduler/database/SchedulerDatabaseBuilder.java
+++ app/src/main/java/com/example/student_scheduler/database/SchedulerDatabaseBuilder.java
@@ -2,11 +2,9 @@ package com.example.student_scheduler.database;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.student_scheduler.dao.AssessmentDAO;
 import com.example.student_scheduler.dao.CourseDAO;
